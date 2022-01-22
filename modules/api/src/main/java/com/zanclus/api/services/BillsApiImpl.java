d5ab448a10f20f01510779735655057a8c7f4ed9
Deven Phillips <deven.phillips@redhat.com>
2022-01-22 09:44:36 -0500

    Finish implementing Bill/Invoid APIs

--- modules/api/src/main/java/com/zanclus/api/services/BillsApiImpl.java
+++ modules/api/src/main/java/com/zanclus/api/services/BillsApiImpl.java
@@ -1,66 +1,28 @@
 package com.zanclus.api.services;
 
 import com.zanclus.models.Bill;
-import com.zanclus.models.Errors;
 import com.zanclus.models.NewBill;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.tuples.Functions;
 import io.smallrye.mutiny.vertx.UniHelper;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
 import org.hibernate.reactive.mutiny.Mutiny;
 
 import javax.persistence.NoResultException;
-import java.io.Serializable;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
-public class BillsApiImpl implements BillsApi {
-    
+public class BillsApiImpl extends AbstractService implements BillsApi {
+
     Mutiny.SessionFactory sessionFactory;
 
     public BillsApiImpl(Mutiny.SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-    }
-
-    private ServiceResponse mapListToServiceResponse(List<? extends Serializable> a) {
-        List<JsonObject> results = a.stream().map(JsonObject::mapFrom).toList();
-        return ServiceResponse.completedWithJson(new JsonArray(results));
-    }
-
-    private ServiceResponse mapEntityToServiceResponse(Serializable b) {
-        return ServiceResponse.completedWithJson(JsonObject.mapFrom(b));
-    }
-
-    private ServiceResponse mapThrowableToServiceResponse(Throwable t) {
-        Errors err = new Errors();
-        err.setMsg(t.getLocalizedMessage());
-        err.setCode(500);
-        err.setTimestamp(LocalDateTime.now());
-        return ServiceResponse.completedWithJson(JsonObject.mapFrom(err));
-    }
-
-    private ServiceResponse mapNoResultToNotFound(Throwable e) {
-        var err = new Errors().code(404).msg("Not found").timestamp(LocalDateTime.now());
-        return ServiceResponse.completedWithJson(JsonObject.mapFrom(err));
-    }
-
-    private Uni<? extends Serializable> mapNullToNotFound(Serializable e) {
-        if (e == null) {
-            return Uni.createFrom().failure(new NoResultException());
-        }
-        return Uni.createFrom().item(e);
-    }
-
-    private ServiceResponse mapToNoContentResponse(Void v) {
-        return new ServiceResponse().setStatusCode(204).setStatusMessage("NO CONTENT");
     }
 
     @Override
@@ -83,33 +45,26 @@ public class BillsApiImpl implements BillsApi {
     @Override
     public void getAllBills(LocalDate startDate, LocalDate endDate, ServiceRequest ctx,
             Handler<AsyncResult<ServiceResponse>> handler) {
-        final var now = LocalDate.now();
-        
-        if (startDate == null) {
-            startDate = now.minusDays(now.getDayOfMonth() - 1L);
-        }
-        
-        if (endDate == null) {
-            endDate = now.plusMonths(3);
-        }
-        
-        if (endDate.isBefore(startDate)) {
-            JsonObject errBody = new JsonObject();
-            errBody.put("message", "End data cannot be before start date in query parameters");
-            errBody.put("code", 400);
-            ServiceResponse badRequest = new ServiceResponse();
-            badRequest.setStatusCode(HttpResponseStatus.BAD_REQUEST.code());
-            badRequest.setStatusMessage(HttpResponseStatus.BAD_REQUEST.reasonPhrase());
-            badRequest.setPayload(errBody.toBuffer());
-            handler.handle(Future.succeededFuture(badRequest));
-        } else {
-            final LocalDate finalStartDate = startDate;
-            final LocalDate finalEndDate = endDate;
-            UniHelper.toFuture(sessionFactory
+        Functions.Function3<LocalDate, LocalDate, Handler<AsyncResult<ServiceResponse>>, Future<ServiceResponse>> fun =
+            (LocalDate start, LocalDate end, Handler<AsyncResult<ServiceResponse>> hdlr) ->
+                  UniHelper.toFuture(sessionFactory
                     .withSession(session -> session.createNamedQuery("getBillsForPeriod", Bill.class)
-                        .setParameter(1, finalStartDate).setParameter(2, finalEndDate).getResultList())
-                    .map(this::mapListToServiceResponse).onFailure().recoverWithItem(this::mapThrowableToServiceResponse))
-                .onComplete(handler);
+                            .setParameter(1, start).setParameter(2, end).getResultList())
+                    .map(this::mapListToServiceResponse).onFailure()
+                    .recoverWithItem(this::mapThrowableToServiceResponse)).onComplete(handler);
+        checkDates(startDate, endDate, fun, handler);
+    }
+    
+    @Override
+    public void updateBill(String id, Bill bill, ServiceRequest ctx, Handler<AsyncResult<ServiceResponse>> handler) {
+        if (bill.getId().toString().contentEquals(id)) {
+            UniHelper.toFuture(sessionFactory.withSession(
+                    session -> session.merge(bill)
+                )
+                .map(this::mapEntityToServiceResponse)
+                .onFailure().recoverWithItem(this::mapThrowableToServiceResponse)).onComplete(handler);
+        } else {
+            handleBadRequest(handler);
         }
     }
 }
--- modules/api/src/main/java/com/zanclus/api/services/IncomeApiImpl.java
+++ modules/api/src/main/java/com/zanclus/api/services/IncomeApiImpl.java
@@ -1,23 +1,47 @@
 package com.zanclus.api.services;
 
+import com.zanclus.models.Bill;
 import com.zanclus.models.Income;
 import com.zanclus.models.NewIncome;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.tuples.Functions;
+import io.smallrye.mutiny.vertx.UniHelper;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
+import org.hibernate.reactive.mutiny.Mutiny;
 
-public class IncomeApiImpl implements IncomeApi {
+import javax.persistence.NoResultException;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class IncomeApiImpl extends AbstractService implements IncomeApi {
+
+    Mutiny.SessionFactory sessionFactory;
+
+    public IncomeApiImpl(Mutiny.SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public void addIncomeSource(NewIncome newIncome, ServiceRequest ctx,
             Handler<AsyncResult<ServiceResponse>> handler) {
-
+        UniHelper.toFuture(sessionFactory.withSession(session -> session.persist(newIncome)).replaceWith(newIncome)
+                .map(this::mapEntityToServiceResponse).map(sr -> sr.setStatusCode(200).setStatusMessage("OK"))
+                .onFailure().recoverWithItem(this::mapThrowableToServiceResponse)).onComplete(handler);
     }
 
     @Override
     public void deleteIncome(String id, ServiceRequest ctx, Handler<AsyncResult<ServiceResponse>> handler) {
-
+        UniHelper
+                .toFuture(sessionFactory.withSession(
+                        session -> session.find(Income.class, UUID.fromString(id)).chain(this::mapNullToNotFound)
+                                .chain(session::remove).chain(session::flush).map(this::mapToNoContentResponse)
+                                .onFailure(NoResultException.class).recoverWithItem(this::mapNoResultToNotFound)))
+                .onComplete(handler);
     }
 
     @Override
@@ -28,13 +52,29 @@ public class IncomeApiImpl implements IncomeApi {
     }
 
     @Override
-    public void getIncomeSources(ServiceRequest ctx, Handler<AsyncResult<ServiceResponse>> handler) {
-
+    public void getIncomeSources(LocalDate startDate, LocalDate endDate, ServiceRequest ctx,
+            Handler<AsyncResult<ServiceResponse>> handler) {
+        
+        Functions.Function3<LocalDate, LocalDate, Handler<AsyncResult<ServiceResponse>>, Future<ServiceResponse>> fun =
+            (LocalDate start, LocalDate end, Handler<AsyncResult<ServiceResponse>> hdlr) -> UniHelper.toFuture(sessionFactory
+                .withSession(session -> session.createNamedQuery("getIncomeForPeriod", Bill.class)
+                    .setParameter(1, start).setParameter(2, end).getResultList())
+                .map(this::mapListToServiceResponse).onFailure()
+                .recoverWithItem(this::mapThrowableToServiceResponse)).onComplete(handler);
+        checkDates(startDate, endDate, fun, handler);
     }
-
+    
     @Override
     public void updateIncome(String id, Income income, ServiceRequest ctx,
             Handler<AsyncResult<ServiceResponse>> handler) {
-
+        if (income.getId().toString().contentEquals(id)) {
+            UniHelper.toFuture(sessionFactory.withSession(
+                    session -> session.merge(income)
+                )
+                .map(this::mapEntityToServiceResponse)
+                .onFailure().recoverWithItem(this::mapThrowableToServiceResponse)).onComplete(handler);
+        } else {
+            handleBadRequest(handler);
+        }
     }
 }
