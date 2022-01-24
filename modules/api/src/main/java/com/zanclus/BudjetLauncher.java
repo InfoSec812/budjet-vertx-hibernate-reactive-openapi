2d7a85188bf3b650b5975a47285d9786067aeb80
Deven Phillips <deven.phillips@redhat.com>
2022-01-24 08:16:38 -0500

    Delegate Vert.x logging to SLF4J

--- modules/api/src/main/java/com/zanclus/BudjetLauncher.java
+++ modules/api/src/main/java/com/zanclus/BudjetLauncher.java
@@ -1,10 +1,11 @@
 package com.zanclus;
 
-public class Launcher extends io.vertx.core.Launcher {
+public class BudjetLauncher extends io.vertx.core.Launcher {
 	
 	public static void main(String[] args) {
 		System.setProperty("vertx.setClassPathResolvingEnabled", "true");
 		System.setProperty("vertx.setFileCachingEnabled", "true");
+		System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
 		
 		new io.vertx.core.Launcher().dispatch(args);
 	}
