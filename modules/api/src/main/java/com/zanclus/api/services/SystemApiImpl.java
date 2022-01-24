4c892d20c621ae25b17bb84227b1a37b621f399c
Deven Phillips <deven.phillips@redhat.com>
2022-01-23 20:42:31 -0500

    Initial implementation of health check

--- modules/api/src/main/java/com/zanclus/api/services/SystemApiImpl.java
+++ modules/api/src/main/java/com/zanclus/api/services/SystemApiImpl.java
@@ -1,8 +1,11 @@
 package com.zanclus.api.services;
 
+import com.zanclus.models.Errors;
+import io.smallrye.mutiny.vertx.UniHelper;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
 import org.hibernate.reactive.mutiny.Mutiny;
@@ -23,7 +26,19 @@ public class SystemApiImpl implements SystemApi, ServiceInterface {
     
     @Override
     public void checkHealth(ServiceRequest ctx, Handler<AsyncResult<ServiceResponse>> handler) {
-
+        Errors healthStatus = new Errors();
+        healthStatus.setCode(500);
+        healthStatus.setMsg("Not ready");
+        UniHelper.toFuture(sessionFactory.withSession(
+                    session -> session.createNativeQuery("SELECT 1").getResultList())
+               .map(o -> {
+                   healthStatus.setCode(200);
+                   healthStatus.setMsg("Ready");
+                   return healthStatus;
+               })
+               .map(h -> ServiceResponse.completedWithJson(healthStatus.toJson())
+           )
+        ).onComplete(handler);
     }
 
     @Override
