20a777975cb2ba5f57876fa454a298e8b0cc93ca
Deven Phillips <deven.phillips@redhat.com>
2022-01-23 19:57:07 -0500

    Using named query, implemented CashFlow service method

--- modules/api/src/main/java/com/zanclus/api/services/CashflowApiImpl.java
+++ modules/api/src/main/java/com/zanclus/api/services/CashflowApiImpl.java
@@ -1,8 +1,12 @@
 package com.zanclus.api.services;
 
+import io.smallrye.mutiny.tuples.Functions;
+import io.smallrye.mutiny.vertx.UniHelper;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
 import org.hibernate.reactive.mutiny.Mutiny;
@@ -10,8 +14,9 @@ import org.hibernate.reactive.mutiny.Mutiny;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
-public class CashflowApiImpl implements CashflowApi {
+public class CashflowApiImpl extends AbstractService implements CashflowApi {
     
     final Vertx vertx;
     
@@ -25,11 +30,33 @@ public class CashflowApiImpl implements CashflowApi {
         this.vertx = vertx;
     }
     
+    private List<JsonObject> mapRawObjectToJsonObject(List<Object> objects) {
+        return objects.stream()
+            .map(o -> new JsonObject((String)o))
+            .toList();
+    }
+    
     @Override
     public void getCashFlow(String startDate, String endDate, Float startingBalance, ServiceRequest ctx,
             Handler<AsyncResult<ServiceResponse>> handler) {
         var parsedStartDate = LocalDate.parse(startDate, DateTimeFormatter.ISO_LOCAL_DATE);
         var parsedEndDate = LocalDate.parse(endDate, DateTimeFormatter.ISO_LOCAL_DATE);
-
+    
+    
+        Functions.Function3<LocalDate, LocalDate, Handler<AsyncResult<ServiceResponse>>, Future<ServiceResponse>> fun =
+            (LocalDate start, LocalDate end, Handler<AsyncResult<ServiceResponse>> hdlr) ->
+                UniHelper.toFuture(sessionFactory
+                    .withSession(session ->
+                         session
+                                .createNamedQuery("getCashFlowForDateRangeAndStartingBalance")
+                                .setParameter(1, start)
+                                .setParameter(2, end)
+                                .setParameter(3, startingBalance)
+                                .getResultList()
+                    )
+                    .map(this::mapRawObjectToJsonObject)
+                    .map(this::mapJsonListToServiceResponse).onFailure()
+                    .recoverWithItem(this::mapThrowableToServiceResponse)).onComplete(handler);
+        checkDates(parsedStartDate, parsedEndDate, fun, handler);
     }
 }
